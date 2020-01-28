417c4fdd3c4d1e59a55b9cbc458c73f74bfa420a
smcmurtry <smcmurtry@gmail.com>
2020-01-28 11:41:16 -0500

    not working yet

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,51 @@
+const webpack = require('webpack')
 const path = require('path')
+const FileListPlugin = require('./FileListPlugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env, argv) => {
-  const { getConfig } = require('@cdssnc/webpack-starter')
-  const config = getConfig({
+  console.info('running in mode:', argv.mode)
+
+  let config = {
     mode: argv.mode,
     entry: {
       styles: './assets/scss/app.scss',
       personal: './routes/personal/js/personal.js',
+      book: './routes/book/js/book.js',
     },
     output: {
-      filename: 'js/[name].[chunkhash].js',
+      filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'public/dist'),
     },
-    stats: 'errors-only',
-  })
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+    },
+    plugins: [
+      new FileListPlugin({ options: true }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+      }),
+      new CleanWebpackPlugin(),
+    ],
+  }
 
   return config
 }
