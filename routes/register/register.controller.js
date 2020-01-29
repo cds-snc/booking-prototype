97266c0450d4781cb5c80ff381886df418a93f38
smcmurtry <smcmurtry@gmail.com>
2020-01-28 23:21:14 -0500

    hooked up hasura api to postres database, now reading that to check credentials

--- routes/register/register.controller.js
+++ routes/register/register.controller.js
@@ -1,12 +1,21 @@
 const { routeUtils } = require('./../../utils')
 const { Schema } = require('./schema.js')
 
+const registerUser = (req, res, next) => {
+  // send data to db
+  // req.session.formdata.fullname
+  // req.session.formdata.email
+  // req.session.formdata.password
+  next()
+}
+
 module.exports = (app, route) => {
   const name = route.name
 
   route.draw(app)
     .get((req, res) => {
+
       res.render(name, routeUtils.getViewData(req, {}))
     })
-    .post(route.applySchema(Schema), route.doRedirect())
+    .post(route.applySchema(Schema), registerUser, route.doRedirect())
 }
--- utils/auth.helpers.js
+++ utils/auth.helpers.js
@@ -2,21 +2,37 @@
 // bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
 //   // Store hash in your password DB.
 // });
+const { GraphQLClient } = require('graphql-request')
 
-const allUsers = [
-    {email: "bob", password: "bob"},
-    {email: "ted", password: "ted"},
-]
+const queryEmail = (email) => {
+  return `{
+  users(where: {email: {_eq: "${email}"}}) {
+    email
+    fullname
+    password
+  }
+}`
+}
+
+const client = new GraphQLClient(process.env.HASURA_ENDPOINT, {
+  headers: {
+    "x-hasura-admin-secret": process.env.HASURA_SECRET,
+  },
+})
 
 const login = (req, res, next) => {
   const { email, password } = req.session.formdata
-  allUsers.forEach(x => {
-    if (email === x.email && password === x.password) {
-      req.session.token = true
-      req.session.profile = { email: email }
-    }
+  
+  client.request(queryEmail(email)).then(data => {
+    console.log(data)
+    data.users.forEach(x => {
+      if (email === x.email && password === x.password) {
+        req.session.token = true
+        req.session.profile = { email: email }
+      }
+    })
+    next()
   })
-  next()
 };
 
 const checkAuth = (req, res, next) => {
