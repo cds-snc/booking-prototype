cca6d4900f83175c6db3c9626ea6fdddb8c8b67d
smcmurtry <smcmurtry@gmail.com>
2020-02-07 16:23:27 -0500

    made create event use 24hr time, fixed the first date selection, now populating the time dropdown correctly

--- routes/book/js/book.js
+++ routes/book/js/book.js
@@ -1,6 +1,5 @@
 const fetch = window.fetch
 const origin = window.location.origin
-let isBlockedDay;
 
 const fetchItems = async () => {
   const eventId = 3
@@ -10,25 +9,27 @@ const fetchItems = async () => {
   const _eventData = JSON.parse(result)
   const eventData = _eventData.events[0]
   console.log("eventData", eventData)
+  setSchedulerOptions(eventData)
+  window.renderCalendar("schedule-send-at")
+}
 
-  isBlockedDay = (day, state) => {
+fetchItems()
+
+const getIsBlockedDay = (eventData) => {
+  return (day, state) => {
+    const today = new Date()
     const availableDays = eventData.available_days.split(",").map(x => +x)
-    if (state.firstAvailableDate > day) {
+    if (today > day) {
       return true;
     }
     if (availableDays && availableDays.indexOf(day.$W) === -1) {
       return true;
     }
+    return false;
   }
-  setSchedulerOptions()
-  window.renderCalendar("schedule-send-at")
-
 }
-
-fetchItems()
-
 const populateTimes = (startHour, endHour, durationMinutes) => {
-  const midnight = new Date('2018-01-01 00:00:00')
+  const midnight = new Date('2018-01-01 00:00:00') // date doesn't matter
   const apptDuration = durationMinutes*60*1000 // ms
   const times = []
   let iter = midnight
@@ -38,7 +39,6 @@ const populateTimes = (startHour, endHour, durationMinutes) => {
     }
     iter = new Date(iter.getTime() + apptDuration);
   }
-  console.log(times)
   const timeOptions = times.map(x => {
     return {
       val: x.toLocaleTimeString("en-US", {hour12: false}),
@@ -48,21 +48,26 @@ const populateTimes = (startHour, endHour, durationMinutes) => {
   return timeOptions;
 };
 
-const setSchedulerOptions = () => {
+const setSchedulerOptions = (eventData) => {
   window.schedulerOptions = {
     init: data => {
       const { dayjs, defaultState } = data;
-
-      const firstDay = dayjs("2020-02-15");
-      const lastAvailableDate = dayjs(firstDay).add(3, "months");
-
+      const { start_hour, end_hour, duration_minutes, daterange_days } = eventData
+      const today = new Date()
+      let firstDay = new Date()
+      const isBlockedDay = getIsBlockedDay(eventData)
+      const oneDay = 24*60*60*1000
+      while (isBlockedDay(dayjs(firstDay), {})) {
+        firstDay = new Date(firstDay.getTime() + oneDay)
+      }
+      const lastAvailableDate = dayjs(today).add(daterange_days, "days");
       return {
-        today: firstDay,
-        firstAvailableDate: firstDay,
+        today: dayjs(today),
+        firstAvailableDate: dayjs(firstDay),
         lastAvailableDate: lastAvailableDate,
         date: dayjs(firstDay).format("YYYY-MM-DD"),
         time: "",
-        time_values: populateTimes(9, 12, 30),
+        time_values: populateTimes(start_hour, end_hour, duration_minutes),
         isBlockedDay: isBlockedDay,
         selected: [dayjs(firstDay).format("YYYY-MM-DD")],
         focusedDayNum: dayjs(firstDay).format("D"),
