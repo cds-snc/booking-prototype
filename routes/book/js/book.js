ac33b75182aa33456974ec034465ac8276f2916b
smcmurtry <smcmurtry@gmail.com>
2020-02-07 12:00:33 -0500

    now isBlockedDay is working

--- routes/book/js/book.js
+++ routes/book/js/book.js
@@ -25,33 +25,27 @@ fetchItems()
 
 const populateTimes = () => {
   return [
-    { val: "1:00", label: "1:00 AM" },
     { val: "2:00", label: "2:00 AM" },
     { val: "3:00", label: "3:00 AM" },
+    { val: "4:00", label: "4:00 AM" },
   ];
 };
-window.isBlockedDay = day => {
-  const data = {
-    blockedDays: [1, 3, 4],
-    dateRange: 80,
-    availableTimes: [
-      { val: "16:00", label: "4:00 PM" },
-      { val: "17:00", label: "5:00 PM" }
-    ],
-  };
-
-  if (data.blockedDays && data.blockedDays.indexOf(day.$W) > -1) {
+const isBlockedDay = (day, state) => {
+  const blockedDays = [1, 3, 4]
+  if (state.firstAvailableDate > day) {
+    return true;
+  }
+  if (blockedDays && blockedDays.indexOf(day.$W) > -1) {
     return true;
   }
 }
-
 
 window.schedulerOptions = {
   init: data => {
     const { dayjs, defaultState } = data;
 
     const firstDay = dayjs("2020-02-15");
-    lastAvailableDate = dayjs(firstDay).add(3, "months");
+    const lastAvailableDate = dayjs(firstDay).add(3, "months");
 
     return {
       today: firstDay,
@@ -60,15 +54,13 @@ window.schedulerOptions = {
       date: dayjs(firstDay).format("YYYY-MM-DD"),
       time: "",
       time_values: populateTimes(),
+      isBlockedDay: isBlockedDay,
       selected: [dayjs(firstDay).format("YYYY-MM-DD")],
       focusedDayNum: dayjs(firstDay).format("D"),
       updateMessage: "",
       _24hr: "en",
-      errors: ""
+      errors: "",
     };
-  },
-  isBlockedDay: day => {
-    return window.isBlockedDay()
   },
 };
 
